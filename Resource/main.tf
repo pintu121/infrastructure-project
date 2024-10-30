48d338d3148c5280a9f028f12f1dda505dd8248d
pintu121 <pintuwap@outlook.com>
2024-10-30 19:37:47 +0530

    resource create

--- Resource/main.tf
+++ Resource/main.tf
@@ -4,24 +4,24 @@ module "vpc" {
     tiger_subnet = var.tiger_subnet
     subnet_names = var.subnet_names
 }
-module "sg" {
-    source = "./modules/sg"
-    vpc_tiger_out = module.vpc.vpc_tiger_out
+# module "sg" {
+#     source = "./modules/sg"
+#     vpc_tiger_out = module.vpc.vpc_tiger_out
   
-}
+# }
 
-module "ec2" {
-    source = "./modules/ec2"
-    instance_name = var.instance_name
-    sg_tiger_out = module.sg.sg_tiger_out
-    subnet_tiger_out = module.vpc.subnet_tiger_out
-    ami = var.ami
-    instances = var.instances
-}
-module "alb" {
-    source = "./modules/alb"
-    vpc_tiger_out = module.vpc.vpc_tiger_out
-    ec2_tiger_out = module.ec2.ec2_tiger_out
-    subnet_tiger_out = module.vpc.subnet_tiger_out
-    sg_tiger_out = module.sg.sg_tiger_out
-}
+# module "ec2" {
+#     source = "./modules/ec2"
+#     instance_name = var.instance_name
+#     sg_tiger_out = module.sg.sg_tiger_out
+#     subnet_tiger_out = module.vpc.subnet_tiger_out
+#     ami = var.ami
+#     instances = var.instances
+# }
+# module "alb" {
+#     source = "./modules/alb"
+#     vpc_tiger_out = module.vpc.vpc_tiger_out
+#     ec2_tiger_out = module.ec2.ec2_tiger_out
+#     subnet_tiger_out = module.vpc.subnet_tiger_out
+#     sg_tiger_out = module.sg.sg_tiger_out
+# }
